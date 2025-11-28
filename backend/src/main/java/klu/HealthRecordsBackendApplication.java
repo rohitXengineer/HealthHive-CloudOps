d7294a99068fc744defcdcb508ad92a422656c27
rohitXengineer <2300033562@kluniversity.in>
2025-11-28 08:51:17 +0530

    Add backend folder and code

---
+++ backend/src/main/java/klu/HealthRecordsBackendApplication.java
@@ -0,0 +1,13 @@
+package klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthRecordsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthRecordsBackendApplication.class, args);
+	}
+
+}
